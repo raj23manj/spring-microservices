3571fcab07b6d476a226f0eab11d9f5b37f7b0ec
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-08 07:58:50 +0530

    Added Ribbon and running client side load balancing with it

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,14 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+// we can call only one instance running on this port, if there are multiple one ? we use ribbon
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service") // name is the name given in application.properties
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
